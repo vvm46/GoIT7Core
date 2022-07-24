b319cdd6b90be288dc42aee4f94bb3de5fe1e1ad
vasylyk_volodya@ukr.net <555HfyljvsP555>
2022-07-24 16:14:17 +0300

    added module 6

---
+++ module_6_second_part/file_parser.py
@@ -0,0 +1,77 @@
+import sys
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+MY_OTHER = []
+ARCHIVES = []
+
+REGISTER_EXTENSIONS = {
+    'JPEG': JPEG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'ZIP': ARCHIVES
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(filename: str) -> str:
+    # перетворюємо розширення файлу на назву папки .jpg -> JPG
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path) -> None:
+    for item in folder.iterdir():
+        # Якщо це папка то додаємо її зі списку FOLDERS і переходимо до наступного елемента папки
+        if item.is_dir():
+            # перевіряємо, щоб папка не була тією, в яку ми складаємо вже файли.
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                # скануємо цю вкладену папку - рекурсія
+                scan(item)
+            # перейти до наступного елемента в сканованій папці
+            continue
+
+        # Робота з файлом
+        ext = get_extension(item.name)  # взяти розширення
+        fullname = folder / item.name  # взяти повний шлях до файлу
+        if not ext:  # якщо файл не має розширення додати до невідомих
+            MY_OTHER.append(fullname)
+        else:
+            try:
+                # взяти список куди покласти повний шлях до файлу
+                container = REGISTER_EXTENSIONS[ext]
+                EXTENSIONS.add(ext)
+                container.append(fullname)
+            except KeyError:
+                # Якщо ми не реєстрували розширення у REGISTER_EXTENSIONS, то додати до іншого
+                UNKNOWN.add(ext)
+                MY_OTHER.append(fullname)
+
+
+if __name__ == '__main__':
+
+    folder_for_scan = sys.argv[1]
+    print(f'Start in folder {folder_for_scan}')
+
+    scan(Path(folder_for_scan))
+    print(f'Images jpeg: {JPEG_IMAGES}')
+    print(f'Images jpg: {JPG_IMAGES}')
+    print(f'Images svg: {SVG_IMAGES}')
+    print(f'Audio mp3: {MP3_AUDIO}')
+    print(f'Archives: {ARCHIVES}')
+
+    print(f'Types of files in folder: {EXTENSIONS}')
+    print(f'Unknown files of types: {UNKNOWN}')
+
+    print(FOLDERS[::-1])
+
+# запускаємо:  python3 main.py `назва_папки_для_сортування`
---
+++ module_6_second_part/main.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import shutil
+import sys
+import file_parser as parser
+from normalize import normalize
+
+
+def handle_media(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_other(filename: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    filename.replace(target_folder / normalize(filename.name))
+
+
+def handle_archive(filename: Path, target_folder: Path):
+    # Створюємо папку для архіву
+    target_folder.mkdir(exist_ok=True, parents=True)
+    # Створюємо папку куди розпакуємо архів
+    # Беремо суфікс у файла і удаляємо replace(filename.suffix, '')
+    folder_for_file = target_folder / normalize(filename.name.replace(filename.suffix, ''))
+
+    # Створюємо папку для архіву з іменем файлу
+    folder_for_file.mkdir(exist_ok=True, parents=True)
+    try:
+        shutil.unpack_archive(str(filename.resolve()),
+                              str(folder_for_file.resolve()))
+    except shutil.ReadError:
+        print(f'Це не архів {filename}!')
+        folder_for_file.rmdir()
+        return None
+    filename.unlink()
+
+
+def handle_folder(folder: Path):
+    try:
+        folder.rmdir()
+    except OSError:
+        print(f'Помилка видалення папки {folder}')
+
+
+def main(folder: Path):
+    parser.scan(folder)
+    for file in parser.JPEG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPEG')
+    for file in parser.JPG_IMAGES:
+        handle_media(file, folder / 'images' / 'JPG')
+    for file in parser.PNG_IMAGES:
+        handle_media(file, folder / 'images' / 'PNG')
+    for file in parser.SVG_IMAGES:
+        handle_media(file, folder / 'images' / 'SVG')
+    for file in parser.MP3_AUDIO:
+        handle_media(file, folder / 'audio' / 'MP3')
+
+    for file in parser.MY_OTHER:
+        handle_other(file, folder / 'MY_OTHER')
+    for file in parser.ARCHIVES:
+        handle_archive(file, folder / 'archives')
+
+    # Виконуємо реверс списку для того щоб видалити всі папки
+    for folder in parser.FOLDERS[::-1]:
+        handle_folder(folder)
+
+
+if __name__ == '__main__':
+    if sys.argv[1]:
+        folder_for_scan = Path(sys.argv[1])
+        print(f'Start in folder {folder_for_scan.resolve()}')
+        main(folder_for_scan.resolve())
